40c25e27801a60b8a3d1f7588d08350ccf079e40
rohan <rohanjaiswar29@gmail.com>
2023-04-03 17:19:47 +0530

    webthree

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,58 @@
 from django.urls import path
 from . import views #from current folder import views.py
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 urlpatterns = [
      path(' ',view=views.landing, name='landing'),
 ]
